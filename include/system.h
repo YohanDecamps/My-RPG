4c07ec62df533e1c20942568b3e74c737bcd2614
Ethan THIERRY <ethan.thierry@epitech.eu>
2023-04-03 12:59:09 +0200

    doc: updating doxygen documentation

--- include/system.h
+++ include/system.h
@@ -7,13 +7,17 @@
 
 #ifndef SYSTEM_H
     #define SYSTEM_H
+
     #include <SFML/Graphics/Types.h>
 
-    int save_as_jpg(sfRenderTexture *rtexture, char *name);
-    int save_as_png(sfRenderTexture *rtexture, char *name);
-    int save_as_bmp(sfRenderTexture *rtexture, char *name);
-    int save_as_tga(sfRenderTexture *rtexture, char *name);
-
+    /**
+     * @brief This function returns the value of the environment variable name,
+                or NULL if the variable is not defined.
+     *
+     * @param env - environment variables array (Should never be NULL)
+     * @param name - name of the environment variable (Should never be NULL)
+     * @return char* - value of the environment variable
+     */
     char *my_getenv(char **env, char *name);
 
 #endif /* !SYSTEM_H */
