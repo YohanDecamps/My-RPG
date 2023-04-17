a32e419eb891793afbe955e880a4fc0d070dca5a
Ethan THIERRY <ethan.thierry@epitech.eu>
2023-04-17 14:59:02 +0200

    docs: updating docs for the dialogs lib

--- include/dialogs.h
+++ include/dialogs.h
@@ -15,10 +15,32 @@
     #define NAME_POSITION (sfVector2u) {310, 460}
     #define TEXT_POSITION (sfVector2u) {100, 350}
 
-    dialog_t *create_dialog(dialog_t *dialogs, char *name, char *text, int time);
+    /**
+     * @brief Create a dialog object from a name, a text and a time
+     *
+     * @param dialogs The current piled dialogs
+     * @param name The name of the dialog
+     * @param text The text of the dialog
+     * @param time The time the dialog will be displayed
+     * @return dialog_t* The new dialog
+     */
+    dialog_t *create_dialog(dialog_t *dialogs,
+                            char *name,
+                            char *text,
+                            int time);
 
+    /**
+     * @brief Initialize the dialog sprite
+     *
+     * @return sfSprite* The dialog sprite
+     */
     sfSprite *init_dialog_sprite(void);
 
+    /**
+     * @brief Display the dialogs
+     *
+     * @param rpg The rpg_t structure
+     */
     void display_dialogs(rpg_t *rpg);
 
 #endif /* !DIALOGS_H_ */
