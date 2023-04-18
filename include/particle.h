238aaafbd9c9fe58d753ecb2b02779568c5466d9
Eliot <eliot1.martin@epitech.eu>
2023-04-18 13:04:13 +0200

    [FIX] coding style in doxygen documentation

--- include/particle.h
+++ include/particle.h
@@ -15,17 +15,20 @@
 
     /**
      * @brief this function updates the particle each frame and print them
-     * 
+     *
      * @param particle = head of the particle list
-     * @param move = the absolute movement desired for the particle, leave at 0, 0 if no movement is desired
+     * @param move = the absolute movement desired for the particle,
+     * leave at 0, 0 if no movement is desired
      * @param w = render window for display
      */
-    void update_particles (verlet_t **particle, sfVector2f move, sfRenderWindow *w);
+    void update_particles (verlet_t **particle, sfVector2f move,
+    sfRenderWindow *w);
 
     /**
      * @brief create a new particle list with the arguments specified in args
-     * 
-     * @param args = the arguments of the function, every modifiable parameter is in it
+     *
+     * @param args = the arguments of the function,
+     * every modifiable parameter is in it
      * @return verlet_t** = the head of the particle list
      */
     verlet_t **create_particles (particles_args_t *args);
