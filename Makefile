2a94b22241e5bfbfc0a23e17160523e09de036ca
Quentin Leguay <quentin.leguay@epitch.eu>
2023-04-20 18:52:01 +0200

    [FIX] coding style

--- Makefile
+++ Makefile
@@ -5,23 +5,23 @@
 ## make file
 ##
 
-SRC	=	src/main.c						\
-		src/my_rpg.c					\
-		src/system/get_usr_input.c		\
-    	src/system/getenv.c				\
-		src/system/get_mouse_pos.c		\
-		src/handle_movement.c			\
-		src/draw_map.c					\
-		src/camera_movement.c			\
-		src/display_framerate.c			\
-		src/init_rpg_values.c			\
-		src/init_rpg_visuals.c			\
-		src/ray_cast/draw_ray_cast.c	\
-		src/ray_cast/utils_ray_cast.c	\
-		src/ray_cast/get_hit_point.c	\
-		src/init_entity.c				\
-		src/animate_entities.c			\
-		src/init_other_rpg_vars.c \
+SRC	=	src/main.c							\
+		src/my_rpg.c						\
+		src/system/get_usr_input.c			\
+    	src/system/getenv.c					\
+		src/system/get_mouse_pos.c			\
+		src/handle_movement.c				\
+		src/draw_map.c						\
+		src/camera_movement.c				\
+		src/display_framerate.c				\
+		src/init_rpg_values.c				\
+		src/init_rpg_visuals.c				\
+		src/ray_cast/draw_ray_cast.c		\
+		src/ray_cast/utils_ray_cast.c		\
+		src/ray_cast/get_hit_point.c		\
+		src/init_entity.c					\
+		src/animate_entities.c				\
+		src/init_other_rpg_vars.c 			\
 		src/menu/draw_all_buttons.c			\
 		src/menu/draw_button.c 				\
 		src/menu/init_all_buttons.c 		\
@@ -34,20 +34,21 @@ SRC	=	src/main.c						\
 		src/menu/assign_keybind.c			\
 		src/reinit_rpg_values.c				\
 		src/menu/update_all_buttons.c 		\
-		src/menu/update_button.c \
-		src/particle.c \
-		src/hit_entity.c \
+		src/menu/update_button.c 			\
+		src/particle.c 						\
+		src/hit_entity.c					\
+		src/ray_cast/cot.c					\
 		src/system/saves/fetch_save.c		\
 		src/system/saves/getvalue.c			\
 		src/system/saves/setvalue.c			\
 		src/system/saves/delvalue.c			\
 		src/system/saves/freesave.c			\
-		src/system/saves/writesave.c \
-		src/switch_level.c \
-		src/freemem.c \
-		src/dialogs/create_dialogs.c \
-		src/dialogs/display_dialogs.c \
-		src/dialogs/init_dialogs.c \
+		src/system/saves/writesave.c		\
+		src/switch_level.c 					\
+		src/freemem.c 						\
+		src/dialogs/create_dialogs.c 		\
+		src/dialogs/display_dialogs.c 		\
+		src/dialogs/init_dialogs.c 			\
 		src/dialogs/free_dialogs.c
 
 TESTS_SRC = ./tests/tests_rpg.c
--- src/ray_cast/utils_ray_cast.c
+++ src/ray_cast/utils_ray_cast.c
@@ -22,11 +22,6 @@ int is_vertical(float xprd, float yprd)
     return (0);
 }
 
-double cot(double x)
-{
-    return (1 / tan(x));
-}
-
 float angle_between(sfVector2f p1, sfVector2f p2)
 {
     float dx = p2.x - p1.x;
@@ -43,6 +38,25 @@ float reset_angle(float angle)
     return (angle);
 }
 
+void change_entity_position_inner(rpg_t *rpg, int nb, ray_cast_t *rc,
+float angle)
+{
+    if (angle > rpg->slope && angle < rpg->slope + (0.0005 * (1080 /
+    rpg->size_y)))
+        sfSprite_setPosition(rpg->entity[0].sprite, (sfVector2f)
+        {(rpg->size_x / 2) + nb, rpg->y_offset});
+    if (rpg->clone_slope + -(rpg->size_x / 2) * (0.0005 * (1080 /
+    rpg->size_y)) > angle
+    || rpg->clone_slope + (rpg->size_x / 2) * (0.0005 * (1080 /
+    rpg->size_y)) < angle || rpg->entity[0].pv <= 0)
+        sfSprite_setPosition(rpg->entity[0].sprite,
+        (sfVector2f) {10000 + nb, rpg->y_offset});
+    if (((rc->x_offset + 1) / rpg->size_x) >= rpg->entity[0].slope
+    && ((rc->x_offset - 1) / rpg->size_x) < rpg->entity[0].slope)
+        sfSprite_setPosition(rpg->entity[0].sprite,
+        (sfVector2f) {(rpg->size_x / 2) + rc->x_offset, rpg->y_offset});
+}
+
 void change_entity_position(rpg_t *rpg, int nb, ray_cast_t *rc)
 {
     float angle = angle_between(rpg->player_pos, rpg->entity[0].pos);
@@ -54,15 +68,5 @@ void change_entity_position(rpg_t *rpg, int nb, ray_cast_t *rc)
         shades = 255;
     sfSprite_setColor(rpg->entity[0].sprite, (sfColor)
     {255 - shades, 255 - shades, 255 - shades, 255});
-    if (angle > rpg->slope && angle < rpg->slope + (0.0005 * (1080 / rpg->size_y)))
-        sfSprite_setPosition(rpg->entity[0].sprite, (sfVector2f)
-        {(rpg->size_x / 2) + nb, rpg->y_offset});
-    if (rpg->clone_slope + -(rpg->size_x / 2) * (0.0005 * (1080 / rpg->size_y)) > angle
-    || rpg->clone_slope + (rpg->size_x / 2) * (0.0005 * (1080 / rpg->size_y)) < angle || rpg->entity[0].pv <= 0)
-        sfSprite_setPosition(rpg->entity[0].sprite,
-        (sfVector2f) {10000 + nb, rpg->y_offset});
-    if (((rc->x_offset + 1) / rpg->size_x) >= rpg->entity[0].slope
-    && ((rc->x_offset - 1) / rpg->size_x) < rpg->entity[0].slope)
-        sfSprite_setPosition(rpg->entity[0].sprite,
-        (sfVector2f) {(rpg->size_x / 2) + rc->x_offset, rpg->y_offset});
+    change_entity_position_inner(rpg, nb, rc, angle);
 }
